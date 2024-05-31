5a1c789bc1e26dfa2783b14888cb35de4c611783
Baglan <bagl0419@gmail.com>
2024-05-31 13:46:40 +0500

    init

---
+++ src/main/java/bitlab/final_project/controller/HomeController.java
@@ -0,0 +1,37 @@
+package bitlab.final_project.controller;
+
+import bitlab.final_project.entity.User;
+import bitlab.final_project.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final UserService userService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/")
+    public String homePage() {
+        return "welcome";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/forbidden")
+    public String forbiddenPage() {
+        return "forbidden";
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    @GetMapping("/profile")
+    public String profilePage() {
+        return "profile";
+    }
+}
